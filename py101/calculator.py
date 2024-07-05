23b523e7db18f5e8a20465dc8e377fcf020c8f84
Justin Yang <yjust16@gmail.com>
2024-07-05 16:12:07 -0700

    Refactor calculator program

--- py101/calculator.py
+++ py101/calculator.py
@@ -4,40 +4,62 @@
 # Perform the operation on the two numbers.
 # Print the result to the terminal.
 
-print('Welcome to Calculator!')
+def prompt(message):
+    print(f'==> {message}')
 
-print("What's the first number")
+def invalid_number(number_str):
+    try:
+        int(number_str)
+    except ValueError:
+        return True
+
+    return False
+
+prompt('Welcome to Calculator!')
+
+prompt("What's the first number")
 number1 = input()
-print("What's the second number")
+
+while invalid_number(number1):
+    prompt('Hmm... that doesn\'t look like a valid number')
+    number1 = input()
+
+prompt("What's the second number")
 number2 = input()
 
-print(f'{number1} {number2}')
+while invalid_number(number2):
+    prompt('Hmm... that doesn\'t look like a valid number')
+    number2 = input()
 
-print('What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide')
+prompt(f'{number1} {number2}')
+
+prompt("""What operation would you like to perform?
+1) Add 2) Subtract 3) Multiply 4) Divide""")
 operation = input()
 
-if operation == '1':
-    output = int(number1) + int(number2)
-elif operation == '2':
-    output = int(number1) - int(number2)
-elif operation == '3':
-    output = int(number1) * int(number2)
-elif operation == '4':
-    output = int(number1) / int(number2)
+# if operation == '1':
+#     output = int(number1) + int(number2)
+# elif operation == '2':
+#     output = int(number1) - int(number2)
+# elif operation == '3':
+#     output = int(number1) * int(number2)
+# elif operation == '4':
+#     output = int(number1) / int(number2)
+
+while operation not in ['1', '2', '3', '4']:
+    prompt('You must choose 1, 2, 3, 4')
+    operation = input()
 
 # Using match/case
 
-# match operation:
-#     case '1':
-#         output = int(number1) + int(number2)
-#     case '2':
-#         output = int(number1) - int(number2)
-#     case '3':
-#         output = int(number1) * int(number2)
-#     case '4':
-#         output = int(number1) / int(number2)
-    
+match operation:
+    case '1':
+        output = int(number1) + int(number2)
+    case '2':
+        output = int(number1) - int(number2)
+    case '3':
+        output = int(number1) * int(number2)
+    case '4':
+        output = int(number1) / int(number2)
 
-
-print(f'The result is {output}')
-print(type(output))
+prompt(f'The result is {output}')
