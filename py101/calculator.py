c872cc93376bc9a8c67dde02e19150b058476a32
Justin Yang <yjust16@gmail.com>
2024-07-06 22:15:17 -0700

    Put text into json file

--- py101/calculator.py
+++ py101/calculator.py
@@ -4,6 +4,11 @@
 # Perform the operation on the two numbers.
 # Print the result to the terminal.
 
+import json
+
+with open('calculator_messages.json', 'r') as file:
+    data = json.load(file)
+
 def prompt(message):
     print(f'==> {message}')
 
@@ -16,26 +21,23 @@ def invalid_number(number_str):
     return False
 
 while True:
-    prompt('Welcome to Calculator!')
+    prompt(data["introduction"])
 
-    prompt("What's the first number")
+    prompt(data["numbers"][0])
     number1 = input()
 
     while invalid_number(number1):
-        prompt('Hmm... that doesn\'t look like a valid number')
+        prompt(data["invalid"])
         number1 = input()
 
-    prompt("What's the second number")
+    prompt(data["numbers"][1])
     number2 = input()
 
     while invalid_number(number2):
-        prompt('Hmm... that doesn\'t look like a valid number')
+        prompt(data["invalid"])
         number2 = input()
 
-    prompt(f'{number1} {number2}')
-
-    prompt("""What operation would you like to perform?
-    1) Add 2) Subtract 3) Multiply 4) Divide""")
+    prompt(data["operation"])
     operation = input()
 
     # if operation == '1':
@@ -48,7 +50,7 @@ while True:
     #     output = int(number1) / int(number2)
 
     while operation not in ['1', '2', '3', '4']:
-        prompt('You must choose 1, 2, 3, 4')
+        prompt(data["operation_choice"])
         operation = input()
 
     # Using match/case
@@ -63,9 +65,8 @@ while True:
         case '4':
             output = int(number1) / int(number2)
 
-    prompt(f'The result is {output}')
-    prompt(f"""Would you like to perform another calculation
-    y/n""")
+    prompt(data["result"] + str(output))
+    prompt(data["another_calculation"])
     decision = input()
 
     if decision and decision[0].lower() == 'n':
